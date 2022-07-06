4613119098ff3673ded948bb7961c8973230f79b
kawamuragen <64403862+kawamuragen@users.noreply.github.com>
2022-07-07 00:49:17 +0900

    Up to lesson 34.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li タグ生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p タグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // li タグの子要素にdivタグ,pタグを挿入
+  const fragment = document.createDocumentFragment();
+  fragment.append(p);
+  fragment.append(completeButton);
+  fragment.append(deleteButton);
+  // console.log(fragment);
+  li.appendChild(div).appendChild(fragment);
+  // console.log(li);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
